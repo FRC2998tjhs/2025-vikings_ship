0ece385197eae362d61a857b4d246b18a3833cff
Main Computer <myskalmuskal@gmail.com>
2025-02-13 17:58:30 -0500

    Get camera's position

--- src/main/java/frc/robot/AprilTagPose.java
+++ src/main/java/frc/robot/AprilTagPose.java
@@ -3,6 +3,8 @@ package frc.robot;
 import edu.wpi.first.apriltag.AprilTagDetection;
 import edu.wpi.first.apriltag.AprilTagPoseEstimate;
 import edu.wpi.first.apriltag.AprilTagPoseEstimator;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
 
 public class AprilTagPose {
     public AprilTagDetection detection;
@@ -16,5 +18,11 @@ public class AprilTagPose {
     public static AprilTagPose fromDetection(AprilTagDetection d, AprilTagPoseEstimator estimator) {
         var pose = estimator.estimateOrthogonalIteration(d, 2);
         return new AprilTagPose(d, pose);
+    }
+
+    public Transform3d bestPose() {
+        var whichPose = pose.error1 < pose.error2 ? pose.pose1 : pose.pose2;
+        // return new Pose3d(whichPose.getTranslation(), whichPose.getRotation());
+        return whichPose;
     }
 }
--- src/main/java/frc/robot/RobotTransform.java
+++ src/main/java/frc/robot/RobotTransform.java
@@ -2,9 +2,14 @@ package frc.robot;
 
 import java.util.List;
 
+import org.dyn4j.geometry.Vector3;
+
 import com.studica.frc.AHRS;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 
 public class RobotTransform {
     private AHRS gyro;
@@ -25,9 +30,18 @@ public class RobotTransform {
             if (tagPosition == null) {
                 continue;
             }
+
+            var tagPose = new Pose3d(tagPosition.x, tagPosition.y, tagPosition.z, new Rotation3d());
+            var cameraToAprilTag = pose.bestPose().inverse();
+            var cameraInWorld = tagPose.plus(cameraToAprilTag);
+
+            var cameraTranslation = cameraInWorld.getTranslation();
+            var correctAxes = new Vector3(cameraTranslation.getX(), -cameraTranslation.getZ(),
+                    -cameraTranslation.getY());
+            // TODO(shelbyd): Why do we need to correct the axes?
+            System.out.println(correctAxes);
+
             // tag's known position + pose estimate + camera's position on bot
-            // System.out.println("Ambiguity " + pose.pose.getAmbiguity());
-            // System.out.println("Found tag " + pose.detection.getId() + " at " + tagPosition);
         }
     }
 
